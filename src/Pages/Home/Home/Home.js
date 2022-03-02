40baf53ecef8d51a63a62715aed2c5108d21ecba
Rohul Amin <rohulamin83716@gmail.com>
2022-03-02 21:38:19 +0600

    more about us section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Users from "../../Users/Users";
 import Navigaton from "../Navigation/Navigation";
+import AboutUs from "./AboutUs/AboutUs";
 
 const Home = () => {
   return (
     <div>
       <Navigaton></Navigaton>
+      <AboutUs></AboutUs>
     </div>
   );
 };
