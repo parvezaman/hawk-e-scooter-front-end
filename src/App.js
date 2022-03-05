2e0657860544cb5f7c89209b1437a1d68ba0ec73
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2022-03-06 01:06:24 +0600

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './Pages/Users/Users'
+import Experiance from './Pages/Home/Experiance';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Users />} />
-            <Route path='/users' element={<Users />} />
+            <Route path='/home' element={<Experiance />} />
+            <Route path='/users' element={<Users/>} />
           </Routes>
         </BrowserRouter>
     </div>
