3dda61919b72a02bfeb1f51bbaaaada2ee1da024
unknown <rjibon49@gmail.com>
2022-03-05 20:28:01 +0600

    update by jibon

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Users from "../../Users/Users";
 import Navigaton from "../Navigation/Navigation";
 import AboutUs from "./AboutUs/AboutUs";
+import Experiance from "./Experiance";
 
 const Home = () => {
   return (
     <div>
       <Navigaton></Navigaton>
       <AboutUs></AboutUs>
+      <Experiance />
     </div>
   );
 };
